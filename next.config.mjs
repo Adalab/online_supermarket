da878d487198aba5763b317dc59423af2e6d6f0e
Ana Martinez <amartinezaguilar6@gmail.com>
2025-01-16 19:01:18 +0100

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    basePath: 'online_supermarket',
+    assetPrefix: 'online_supermarket',
+};;
 
 export default nextConfig;
