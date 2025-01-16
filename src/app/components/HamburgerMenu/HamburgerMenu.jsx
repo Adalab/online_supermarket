402e77e8dd4d32de25a339ee84fec19413267a0d
Ana Martinez <amartinezaguilar6@gmail.com>
2025-01-16 18:55:28 +0100

    Init

---
+++ src/app/components/HamburgerMenu/HamburgerMenu.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import styles from './HamburgerMenu.module.css';
+
+export default function HamburgerMenu() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
+    return (
+        <div className={styles.menuContainer}>
+            <div
+                className={`${styles.hamburgerMenu} ${menuOpen ? styles.change : ''}`}
+                onClick={toggleMenu}
+            >
+                <span className={`${styles.line} ${styles.firstLine}`}></span>
+                <span className={`${styles.line} ${styles.secondLine}`}></span>
+            </div>
+
+            {menuOpen && (
+                <div className={styles.menuExpanded}>
+                    <ul className={styles.menuList}>
+                        <li><a href="#">Frutas y Verduras</a></li>
+                        <li><a href="#">Carnicería</a></li>
+                        <li><a href="#">Lácteos</a></li>
+                        <li><a href="#">Higiene Personal</a></li>
+                        <li><a href="#">Panadería</a></li>
+                    </ul>
+                </div>
+            )}
+        </div>
+    );
+}
