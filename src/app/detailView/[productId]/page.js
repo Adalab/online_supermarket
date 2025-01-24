240fdfa4e83de0a83f0509077c4cb43a6208ac78
Ana Martinez <amartinezaguilar6@gmail.com>
2025-01-24 16:25:18 +0100

    Static params

--- src/app/detailView/[productId]/page.js
+++ src/app/detailView/[productId]/page.js
@@ -1,26 +1,32 @@
-"use client";
-import { useParams } from "next/navigation";
-import ProductDetail from "../components/ProductDetail/ProductDetail";
 import productsData from "../../../data/products.json";
 import vegetablesProductsData from "../../../data/vegetables_products.json";
+import ProductDetail from "../../components/ProductDetail/ProductDetail";
 import Header from "@/app/components/Header/Header";
 
-export default function DetailView() {
+export async function generateStaticParams() {
+    const allProducts = Object.values(productsData).flat();
+    const allVegetables = vegetablesProductsData;
 
-    const { productId } = useParams();
+    const allIds = [...allProducts, ...allVegetables].map((product) => ({
+        productId: product.id,
+    }));
+
+    return allIds;
+}
+
+export default function DetailView({ params }) {
+    const { productId } = params;
 
     const allProducts = Object.values(productsData).flat();
     let product = allProducts.find((product) => product.id === productId);
 
     if (!product) {
         product = vegetablesProductsData.find((product) => product.id === productId);
-
     }
 
     return (
         <>
-            <Header>
-            </Header>
+            <Header />
             <div>
                 {product ? (
                     <ProductDetail product={product} />
